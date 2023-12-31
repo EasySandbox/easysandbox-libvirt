117dfe636ad6081a3ddef0771384e6487e7c1e3c
Kevin F <beardog@mailbox.org>
2023-12-31 15:21:04 -0600

    moved xpra logic from main program

---
+++ xpra/xpra.go
@@ -0,0 +1,121 @@
+package xpra
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"git.voidnet.tech/kev/easysandbox-livbirt/sandboxrunning"
+	"git.voidnet.tech/kev/easysandbox/sandbox"
+	"github.com/estebangarcia21/subprocess"
+	"libvirt.org/go/libvirt"
+)
+
+var XPRA_BIN_NAME = "xpra"
+
+func getSandboxXPRAConnectionString(sandboxName string) (string, error) {
+
+	portFileData, portFileErr := os.ReadFile(filepath.Join(sandbox.SandboxInstallDir, sandboxName, "xpra-port"))
+	if portFileErr != nil {
+		return "", portFileErr
+	}
+
+	return fmt.Sprintf("tcp://127.0.0.1:%s", string(portFileData)), nil
+
+}
+
+func isXpraAttached(sandboxName string) (bool, error) {
+	connString, getConnStringErr := getSandboxXPRAConnectionString(sandboxName)
+	if getConnStringErr != nil {
+		return false, getConnStringErr
+	}
+	attachSemaphorePath := filepath.Join(sandbox.SandboxInstallDir, sandboxName, "xpra-attach")
+	semaphoreContents, semaphoreReadErr := os.ReadFile(attachSemaphorePath)
+	if semaphoreReadErr != nil {
+		if os.IsNotExist(semaphoreReadErr) {
+			return false, nil
+		}
+		return false, semaphoreReadErr
+	}
+	if string(semaphoreContents) == connString {
+		return true, nil
+	}
+	os.Remove(attachSemaphorePath)
+	return false, nil
+}
+
+func StartXpraClient(sandboxName string) error {
+
+	conn, err := libvirt.NewConnect("qemu:///session")
+	if err != nil {
+		return fmt.Errorf("error connecting to libvirt: %w", err)
+	}
+
+	for  {
+		sandboxRunning, sandboxRunningErr := sandboxrunning.IsSandboxRunning(sandboxName, conn)
+		if sandboxRunningErr != nil {
+			return sandboxRunningErr
+		}
+		if sandboxRunning {
+			break
+		}
+		time.Sleep(time.Millisecond * 10)
+	}
+
+	xpraAttached, xpraAttachedErr := isXpraAttached(sandboxName)
+	if xpraAttachedErr != nil {
+		return xpraAttachedErr
+	}
+	if xpraAttached {
+		return errors.New("xpra is already attached")
+	}
+
+	connString, getConnStringErr := getSandboxXPRAConnectionString(sandboxName)
+	if getConnStringErr != nil {
+		return getConnStringErr
+	}
+
+	var waitForServerProc *subprocess.Subprocess
+	serverUp := false
+	for !serverUp {
+		waitForServerProc = subprocess.New("xpra", subprocess.Args("control", connString, "hello"))
+		waitForServerProc.Exec()
+		fmt.Println("exit code", waitForServerProc.ExitCode())
+		if waitForServerProc.ExitCode() == 0 {
+			serverUp = true
+		}
+		time.Sleep(time.Millisecond * 20)
+
+	}
+
+	attachSemaphorePath := filepath.Join(sandbox.SandboxInstallDir, sandboxName, "xpra-attach")
+	defer os.Remove(attachSemaphorePath)
+	xpraAttachSemaphoreErr := os.WriteFile(attachSemaphorePath, []byte(connString), 0644)
+	if xpraAttachSemaphoreErr != nil {
+		return xpraAttachSemaphoreErr
+	}
+
+	return subprocess.New("xpra", subprocess.Args("attach", connString, "--splash=no", "--dpi=100")).Exec()
+}
+
+func RunXpraCommand(sandboxName string, args ...string) error {
+
+	xpraAttached, xpraAttachedErr := isXpraAttached(sandboxName)
+	if xpraAttachedErr != nil {
+		return xpraAttachedErr
+	}
+	if !xpraAttached {
+		return errors.New("xpra is not attached")
+	}
+
+	connString, getConnStringErr := getSandboxXPRAConnectionString(sandboxName)
+	if getConnStringErr != nil {
+		return getConnStringErr
+	}
+
+	return subprocess.New("xpra", subprocess.Args("control", connString, "start", strings.Join(args, " "))).Exec()
+
+}
